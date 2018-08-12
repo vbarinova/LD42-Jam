0f53818eca20dfcb244d83caa9fd62d419988670
Victoria B <vbarinova97@gmail.com>
2018-08-12 14:32:48 -0700

    Main Menu Animation, Controls and a Shopping Shelf
    main menu can now Quit Game and also Begin game  yay!

---
+++ Augmented Shopping Simulator/Assets/MainMenuEvents.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuEvents : MonoBehaviour {
+
+    public Canvas fade;
+    public string levelname;
+    public string fade_trigger;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void BeginGame()
+    {
+        Invoke("FadeTrigger", 2);
+        Invoke("loadLevel", 3);
+    }
+
+
+    void loadLevel()
+    {
+        SceneManager.LoadScene(levelname);
+    }
+
+    void FadeTrigger()
+    {
+        fade.GetComponent<Animator>().Play(fade_trigger);
+    }
+
+    public void buttonQuit()
+    {
+        Application.Quit();
+    }
+}
